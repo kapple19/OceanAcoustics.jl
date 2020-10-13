5aa3f35a148de15075fcbecf0276c0e82111d9b3
Aaron <aaronjkaw@gmail.com>
2020-10-14 05:41:39 +1100

    Fixed tests: signature change from Real to Float64

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,31 +4,31 @@ using Test
 
 ## Function Interpolation
 @testset "FunctionInterpolation" begin
-    x = [0, 2e2, 2e3, 5e3]
+    x = [0.0, 2e2, 2e3, 5e3]
     y = [1480.0, 1510.0, 1500.0, 1520.0]
 
     fcn = OceanAcoustics.interpolated_function(x, y)
-    @test fcn(-1) == y[1]
+    @test fcn(-1.0) == y[1]
     for n = 1:length(x)
         @test fcn(x[n]) == y[n]
     end
     @test fcn(x[end] + 1) == y[end]
 
     y = x
-    x = [0, 5e2, 5e3, 10e3, 20e3]
+    x = [0.0, 5e2, 5e3, 10e3, 20e3]
     z = 1500.0 .- x/20e3*100 .+ y'/5e3*200
 
     fcn = OceanAcoustics.interpolated_function(x, y, z)
-    for x₀ ∈ [-1, 0], y₀ ∈ [-1, 0]
+    for x₀ ∈ [-1.0, 0.0], y₀ ∈ [-1.0, 0.0]
         @test fcn(x₀, y₀) == z[1]
     end
-    for x₀ ∈ x[end] .+ [0, 1], y₀ ∈ y[1] .- [0, 1]
+    for x₀ ∈ x[end] .+ [0.0, 1.0], y₀ ∈ y[1] .- [0.0, 1.0]
         @test fcn(x₀, y₀) == z[end, 1]
     end
-    for x₀ ∈ x[1] .- [0, 1], y₀ ∈ y[end] .+ [0, 1]
+    for x₀ ∈ x[1] .- [0.0, 1.0], y₀ ∈ y[end] .+ [0.0, 1.0]
         @test fcn(x₀, y₀) == z[1, end]
     end
-    for x₀ ∈ x[end] .+ [0, 1], y₀ ∈ y[end] .+ [0, 1]
+    for x₀ ∈ x[end] .+ [0.0, 1.0], y₀ ∈ y[end] .+ [0.0, 1.0]
         @test fcn(x₀, y₀) == z[end, end]
     end
     for (nx, x₀) ∈ enumerate(x), (ny, y₀) ∈ enumerate(y)
@@ -38,8 +38,8 @@ end
 
 ## Reflection
 @testset "BoundaryReflection" begin
-    for θ_bnd = deg2rad.(-85:5:85)
-        for θ_inc = deg2rad.(-85:5:85)
+    for θ_bnd = deg2rad.(-85.0:5.0:85.0)
+        for θ_inc = deg2rad.(-85.0:5.0:85.0)
             t_inc = [cos(θ_inc), sin(θ_inc)]
             t_bnd = [cos(θ_bnd), sin(θ_bnd)]
             t_rfl = OceanAcoustics.boundary_reflection(t_inc, t_bnd)
@@ -51,7 +51,7 @@ end
 
 ## Position
 @testset "Position" begin
-    for x ∈ range(0, 10e3, length = 51), y ∈ range(0, 5e3, length = 31)
+    for x ∈ range(0.0, 10e3, length = 51), y ∈ range(0.0, 5e3, length = 31)
         pos = Position(x, y)
         @test pos.r == x
         @test pos.z == y
@@ -68,7 +68,7 @@ end
 
 ## Source
 @testset "Source" begin
-    for x ∈ range(0, 10e3, length = 21), y ∈ range(0, 5e3, length = 11)
+    for x ∈ range(0.0, 10e3, length = 21), y ∈ range(0.0, 5e3, length = 11)
         pos = Position(x, y)
         for f ∈ 10.0.^(0:5)
             sig = Signal(f)
@@ -87,20 +87,20 @@ end
     R = 1e3
     Z = 1e3
 
-    cVal = 1500
+    cVal = 1500.0
     ocn = Medium(cVal, R, Z)
     @test ocn.c(-1, 0) == cVal
     @test ocn.c(R/2, 0) == cVal
     @test ocn.c(R + 1, 0) == cVal
 
-    zVec = range(0, Z, length = 3)
-    cVec = [1550, 1500, 1600]
+    zVec = range(0.0, Z, length = 3)
+    cVec = [1550.0, 1500.0, 1600.0]
     ocn = Medium(zVec, cVec, R, Z)
-    @test ocn.c(0, -1) == cVec[1]
-    @test ocn.c(0, zVec[1]) == cVec[1]
-    @test ocn.c(0, zVec[2]) == cVec[2]
-    @test ocn.c(0, zVec[3]) == cVec[3]
-    @test ocn.c(0, Z + 1) == cVec[3]
+    @test ocn.c(0.0, -1.0) == cVec[1]
+    @test ocn.c(0.0, zVec[1]) == cVec[1]
+    @test ocn.c(0.0, zVec[2]) == cVec[2]
+    @test ocn.c(0.0, zVec[3]) == cVec[3]
+    @test ocn.c(0.0, Z + 1.0) == cVec[3]
 end
 
 ## Ray
