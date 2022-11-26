5337e8c360c68614d776d7319d6d92f9576bec49
kapple19 <aaronjkaw@gmail.com>
2022-11-26 19:42:54 +1100

    Stored trace information in struct

--- scripts/ray.jl
+++ scripts/ray.jl
@@ -2,13 +2,13 @@
 using OceanAcoustics
 
 for scenario in propertynames(examples)[2:end]
-	println()
+	println("="^length(scenario |> string))
 	println(scenario)
 
 	scn = getproperty(examples, scenario)
-	rays = Trace(scn)
+	trc = Trace(scn, π/6 * range(-1.0, 1.0, 11))
 
-	for ray in rays
+	for ray in trc.rays
 		s = range(0.0, ray.s_max, 5)
 		println()
 		@show ray.r.(s)
