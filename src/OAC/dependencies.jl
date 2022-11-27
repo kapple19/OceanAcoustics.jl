994ed586332c6432e10e557bb93a91ddaa4d5708
kapple19 <aaronjkaw@gmail.com>
2022-11-28 00:10:18 +1100

    Surface reflection

--- src/OAC/dependencies.jl
+++ src/OAC/dependencies.jl
@@ -1,7 +1,7 @@
 using Interpolations: linear_interpolation, Line
 using IntervalArithmetic: AbstractInterval, Interval, (..)
 using ForwardDiff: derivative
-using OrdinaryDiffEq: ODEProblem, solve, Tsit5
+using OrdinaryDiffEq: ODEProblem, solve, Tsit5, ContinuousCallback, CallbackSet, terminate!
 using RecipesBase: RecipesBase, @userplot, @recipe, @series
 
 export (..)
--- src/model/ray.jl
+++ src/model/ray.jl
@@ -1,3 +1,18 @@
+dot(u::AbstractVector{<:Number}, v::AbstractVector{<:Number}) = u' * v
+
+function gradient2tangent(m::Float64)
+	x = 1 / √(m^2 + 1)
+	[x; m*x]
+end
+
+function reflection(tng_inc::Vector{Float64}, m_bnd::Float64)
+	tng_bnd = gradient2tangent(m_bnd)
+	nrm_bnd = [0 1; -1 0] * tng_bnd
+	α = dot(tng_inc, nrm_bnd)
+	tng_rfl = tng_inc - 2α * nrm_bnd
+	atan(tng_rfl[2] / tng_rfl[1])
+end
+
 struct Ray <: OAC
 	θ₀
 	r
@@ -23,10 +38,33 @@ struct Trace <: OAC
 			 - 2 * ∂²c_∂r∂z(r, z) * ξ * ζ
 			 + ∂²c_∂z²(r, z) * ξ^2
 		)
+
+		function reflect!(int, bnd)
+			r, z, ξ, ζ = int.u[1:4]
+			c = cel(r, z)
+			tng_inc = c * [ξ; ζ]
+	
+			θ_rfl = reflection(tng_inc, derivative(bnd.z, r))
+	
+			int.u[3] = cos(θ_rfl) / c
+			int.u[4] = sin(θ_rfl) / c
+		end
+	
+		cb_rng = ContinuousCallback(
+			(x, s, int) -> x[1] - scn.ent.rcv.x,
+			terminate!
+		)
+	
+		cb_srf = ContinuousCallback(
+			(x, s, int) -> x[2] - scn.env.srf.z(x[1]),
+			int -> reflect!(int, scn.env.srf)
+		)
+
+		cb = CallbackSet(cb_rng, cb_srf)
 	
 		function tracer!(du, u, params, s)
 			# r, z, ξ, ζ, τ, pRe, pIm, qRe, qIm = u
-			r, z, ξ, ζ = u
+			r, z, ξ, ζ = u[1:4]
 			
 			c = cel(r, z)
 			∂cr = ∂c_∂r(r, z)
@@ -72,7 +110,7 @@ struct Trace <: OAC
 		
 			prob = ODEProblem(tracer!, u₀, s_span)
 		
-			sol = solve(prob, Tsit5())
+			sol = solve(prob, Tsit5(), callback = cb)
 	
 			r(s) = sol(s, idxs = 1)
 			z(s) = sol(s, idxs = 2)
