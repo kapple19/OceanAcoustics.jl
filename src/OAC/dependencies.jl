8487924f919323fee5718168824a89767b46ec97
kapple19 <aaronjkaw@gmail.com>
2022-11-27 22:28:37 +1100

    Specified dependencies

--- src/OAC/dependencies.jl
+++ src/OAC/dependencies.jl
@@ -1,7 +1,7 @@
 using Interpolations: linear_interpolation, Line
 using IntervalArithmetic: AbstractInterval, Interval, (..)
 using ForwardDiff: derivative
-using OrdinaryDiffEq
-using RecipesBase
+using OrdinaryDiffEq: ODEProblem, solve, Tsit5
+using RecipesBase: RecipesBase, @userplot, @recipe, @series
 
 export (..)
