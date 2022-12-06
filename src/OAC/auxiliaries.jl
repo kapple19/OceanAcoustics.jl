eb78875c1b94fec90e70f0be345fb8bf83dda50f
kapple19 <aaronjkaw@gmail.com>
2022-12-06 13:38:37 +1100

    Removed unused function series125

--- src/OAC/auxiliaries.jl
+++ src/OAC/auxiliaries.jl
@@ -1,19 +1,19 @@
-function series125(N::Int)
-	@assert N ≥ 1 "Input must be a positive integer."
-	series = Int[]
-	index = 1
-	while isempty(series) || series[end] < N
-		for n = [1, 2, 5]
-			num = n * index
-			if num > N
-				return series
-			else
-				push!(series, num)
-			end
-		end
-		index *= 10
-	end
-	series
-end
+# function series125(N::Int)
+# 	@assert N ≥ 1 "Input must be a positive integer."
+# 	series = Int[]
+# 	index = 1
+# 	while isempty(series) || series[end] < N
+# 		for n = [1, 2, 5]
+# 			num = n * index
+# 			if num > N
+# 				return series
+# 			else
+# 				push!(series, num)
+# 			end
+# 		end
+# 		index *= 10
+# 	end
+# 	series
+# end
 
-export series125
+# export series125
