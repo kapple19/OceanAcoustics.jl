29adbda633bc0a80f0e46ac610942f5053edbc83
kapple19 <aaronjkaw@gmail.com>
2022-12-12 11:51:30 +1100

    Cleaned up ray method code

--- src/model/ray.jl
+++ src/model/ray.jl
@@ -38,22 +38,6 @@ end
 
 export Ray
 
-# struct Rays <: Oac
-# 	vec_of_rays::Vector{Ray}
-# 	function Rays(rays::AbstractVector{Ray})
-# 		new(rays)
-# 	end
-# end
-
-# export Rays
-
-# function Base.getindex(rays::Rays, n::Int)
-# 	rays.vec_of_rays[n]
-# end
-
-# Base.firstindex(rays::Rays) = 1
-# Base.lastindex(rays::Rays) = length(rays.vec_of_rays)
-
 function default_angles(scn::Scenario, N::Int = 1001)
 	x_rcv = scn.ent.rcv.x
 	rng_ocn = calc_ocean_depth_range(scn)
@@ -70,156 +54,12 @@ function default_angles(scn::Scenario, N::Int = 1001)
 	end
 end
 
-# struct Trace <: Oac
-# 	scn::Scenario
-# 	rays::Vector{Ray}
-# end
-
 struct Trace <: Oac
 	rays::Vector{Ray}
 end
 
 export Trace
 
-# function Trace(
-# 	scn::Scenario,
-# 	angles::AbstractVector{<:AbstractFloat} = default_angles(scn)
-# 	)
-# 	# Computation
-
-# 	cel(r, z) = scn.env.ocn.c(r, z)
-# 	∂c_∂r(r, z) = derivative(r̂ -> cel(r̂, z), r)
-# 	∂c_∂z(r, z) = derivative(ẑ -> cel(r, ẑ), z)
-# 	∂²c_∂r²(r, z) = derivative(r̂ -> ∂c_∂r(r̂, z), r)
-# 	∂²c_∂z²(r, z) = derivative(ẑ -> ∂c_∂z(r, ẑ), z)
-# 	∂²c_∂r∂z(r, z) = derivative(r̂ -> ∂c_∂z(r̂, z), r)
-# 	∂²c_∂n²(r, z, ξ, ζ) = cel(r, z)^2 * (
-# 		∂²c_∂r²(r, z) * ζ^2
-# 		 - 2 * ∂²c_∂r∂z(r, z) * ξ * ζ
-# 		 + ∂²c_∂z²(r, z) * ξ^2
-# 	)
-
-# 	δθ₀ = if length(angles) == 1
-# 		1.0
-# 	else
-# 		angles |> diff |> mean
-# 	end
-
-# 	function reflect!(int, bnd)
-# 		r, z, ξ, ζ = int.u[1:4]
-# 		c = cel(r, z)
-# 		tng_inc = c * [ξ; ζ]
-
-# 		θ_rfl = reflection(tng_inc, derivative(bnd.z, r))
-
-# 		int.u[3] = cos(θ_rfl) / c
-# 		int.u[4] = sin(θ_rfl) / c
-# 	end
-
-# 	cb_rng = ContinuousCallback(
-# 		(x, s, int) -> x[1] - scn.ent.rcv.x,
-# 		terminate!
-# 	)
-
-# 	cb_srf = ContinuousCallback(
-# 		(x, s, int) -> x[2] - scn.env.srf.z(x[1]),
-# 		int -> reflect!(int, scn.env.srf)
-# 	)
-
-# 	cb_btm = ContinuousCallback(
-# 		(x, s, int) -> x[2] - scn.env.btm.z(x[1]),
-# 		int -> reflect!(int, scn.env.btm)
-# 	)
-
-# 	cb = CallbackSet(cb_rng, cb_btm, cb_srf)
-
-# 	function tracer!(du, u, params, s)
-# 		r, z, ξ, ζ, τ, pRe, pIm, qRe, qIm = u
-		
-# 		c = cel(r, z)
-# 		∂cr = ∂c_∂r(r, z)
-# 		∂cz = ∂c_∂z(r, z)
-# 		∂²cnn = ∂²c_∂n²(r, z, ξ, ζ)
-
-# 		du[1] = dr = c * ξ
-# 		du[2] = dz = c * ζ
-# 		du[3] = dξ = -∂cr / c^2
-# 		du[4] = dζ = -∂cz / c^2
-# 		du[5] = dτ = 1/c
-# 		du[6] = dpRe = -∂²cnn / c^2 * qRe
-# 		du[7] = dpIm = -∂²cnn / c^2 * qIm
-# 		du[8] = dqRe = c * pRe
-# 		du[9] = dqIm = c * pIm
-# 	end
-
-# 	f = scn.ent.src.f
-
-# 	r₀ = 0.0
-# 	z₀ = scn.ent.src.z
-# 	c₀ = cel(r₀, z₀)
-# 	τ₀ = 0.0
-# 	p₀Re = 1.0
-# 	p₀Im = 0.0
-# 	λ₀ = c₀ / f
-# 	W₀ = 30λ₀
-# 	q₀Re = 0.0
-# 	q₀Im = pi * f * W₀^2
-
-# 	rays = Ray[]
-# 	for θ₀ in angles
-# 		ξ₀ = cos(θ₀) / c₀
-# 		ζ₀ = sin(θ₀) / c₀
-
-# 		u₀ = [r₀, z₀, ξ₀, ζ₀, τ₀, p₀Re, p₀Im, q₀Re, q₀Im]
-
-# 		s_span = (0.0, 100e3)
-	
-# 		prob = ODEProblem(tracer!, u₀, s_span)
-	
-# 		sol = solve(prob, Tsit5(), callback = cb)
-
-# 		s_max = sol.t[end]
-# 		r(s) = sol(s, idxs = 1)
-# 		z(s) = sol(s, idxs = 2)
-# 		ξ(s) = sol(s, idxs = 3)
-# 		ζ(s) = sol(s, idxs = 4)
-# 		τ(s) = sol(s, idxs = 5)
-# 		p(s) = sol(s, idxs = 6) + im * sol(s, idxs = 7)
-# 		q(s) = sol(s, idxs = 8) + im * sol(s, idxs = 9)
-		
-# 		c(s) = cel(r(s), z(s))
-# 		ω = 2π * f
-# 		A = δθ₀ / c(0.0) * sqrt(
-# 			q(0.0) * f * cos(θ₀)
-# 		) * exp(im * π / 4)
-# 		function p_beam(s::Real, n::Real)
-# 			(s > s_max || s < 0) && return ComplexF64(0.0)
-# 			r(s) < 0 && return ComplexF64(0.0)
-# 			return A * sqrt(
-# 					c(s) / r(s) / q(s)
-# 				) * exp(
-# 				-im * ω * (
-# 					τ(s) + p(s) / 2 / q(s) * n^2
-# 				)
-# 			)
-# 		end
-# 		p_beam(s::Real) = p_beam(s, 0.0)
-
-# 		if save_trace
-# 			push!(rays, Ray(θ₀, s_max, r, z, c, τ, p_beam, TL))
-# 		end
-# 	end
-# 	new(scn, rays)
-# end
-
-# function Trace(
-# 	scn::Scenario,
-# 	N::Int = 1001
-# 	)
-# 	Trace(scn, default_angles(scn, N))
-# end
-
-# User Recipe
 @userplot RayTracePlot
 @recipe function plot(rtp::RayTracePlot)
 	# Parse Inputs
@@ -240,53 +80,6 @@ export Trace
 end
 
 export raytraceplot
-
-# function FieldOld(
-# 	trc::Trace,
-# 	rGrid::AbstractVector{<:Real} = range(0.0, trc.scn.ent.rcv.x, 250),
-# 	zGrid::AbstractVector{<:Real} = range(calc_ocean_depth_range(trc.scn)..., 150)
-# 	)
-
-# 	Nr = length(rGrid)
-# 	Nz = length(zGrid)
-# 	p = zeros(ComplexF64, Nr, Nz)
-# 	Narc = max(101, floor(Nr / 3) |> Int)
-# 	for ray in trc.rays
-# 		arc = range(0.0, ray.s_max, Narc)
-# 		for i = eachindex(arc)[begin+1:end]
-# 			sᵢ₋₁, sᵢ = arc[i-1 : i]
-# 			rᵢ₋₁, rᵢ = ray.r.([sᵢ₋₁, sᵢ])
-# 			# zᵢ₋₁ = ray.z(sᵢ₋₁)
-# 			zᵢ = ray.z(sᵢ)
-# 			for (nr, r) in enumerate(rGrid)
-# 				if !(rᵢ₋₁ .≤ r .< rᵢ)
-# 					continue
-# 				end
-# 				for (nz, z) in enumerate(zGrid)
-# 					x_rcv = [r, z]
-# 					# x_ray = [rᵢ₋₁, zᵢ₋₁]
-# 					x_ray = [rᵢ, zᵢ]
-# 					t_ray = ray.c(sᵢ₋₁) * [ray.ξ(sᵢ₋₁), ray.ζ(sᵢ₋₁)]
-# 					t_ray /= dot(t_ray, t_ray) |> sqrt
-# 					n_ray = ray.c(sᵢ₋₁) * [-ray.ζ(sᵢ₋₁), ray.ξ(sᵢ₋₁)]
-# 					n_ray /= dot(n_ray, n_ray) |> sqrt
-# 					s = dot(x_rcv - x_ray, t_ray)
-# 					n = dot(x_rcv - x_ray, n_ray) |> abs
-# 					p_add = ray.p(sᵢ₋₁ + s, n)
-# 					if !(isnan(p_add) || isinf(p_add))
-# 						p[nr, nz] += p_add
-# 					end
-# 				end
-# 			end
-# 		end
-# 	end
-# 	TL = -20log10.(p .|> abs)
-# 	TL = max.(TL, 0.0)
-# 	TL = min.(TL, 100.0)
-# 	rGrid, zGrid, TL
-# end
-
-# export FieldOld
 
 default_ranges(scn::Scenario) = range(0.0, scn.ent.rcv.x, 250)
 
