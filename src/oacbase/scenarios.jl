edf043ae01a70907bb15ba2e5e415d65f1c3e371
kapple19 <aaronjkaw@gmail.com>
2022-12-20 17:36:40 +1100

    Moved recipe to where it should be

--- src/oacbase/scenarios.jl
+++ src/oacbase/scenarios.jl
@@ -143,43 +143,6 @@ Scenario(scn::Scenario) = scn
 
 Scenario(scn) = Scenario(scn...)
 
-@userplot ScenarioPlot
-@recipe function plot(sp::ScenarioPlot)
-	# Inputs
-	scn = sp.args[1]
-
-	# Plot Design
-	legend --> :none
-	yflip := true
-	
-	# Extrema
-	ocn_depth_range = calc_ocean_depth_range(scn)
-	ex = (
-		srf = minimum(ocn_depth_range),
-		btm = maximum(ocn_depth_range)
-	)
-	ylims --> (ex.srf, ex.btm)
-
-	# Plot Labels
-	plot_title := scn.name
-	xguide := "Range [m]"
-	yguide := "Depth [m]"
-
-	r = range(0.0, scn.ent.rcv.r)
-	# Boundaries
-	for boundary in (:srf, :btm)
-		bnd = getproperty(scn.env, boundary)
-		r = range(0.0, scn.ent.rcv.r)
-		z = bnd.z.(r)
-		@series begin
-			linecolor := :brown
-			fillrange := zeros(size(z)) .+ ex[boundary]
-			fillcolor := :brown
-			r, z
-		end
-	end
-end
-
 function calc_bnd_range(scn::Scenario, bnd::Symbol)
 	r_rng = 0.0 .. scn.ent.rcv.r
 	z_rng = getproperty(scn.env, bnd).z(r_rng)
